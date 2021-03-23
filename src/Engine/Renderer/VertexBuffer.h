257d695d2f174f8a4c7f30b2c2a2f9f376361942
Dane Gottwald <danegottwald@gmail.com>
2021-03-22 20:58:15 -0700

    template constructor for std::array use, added comments

--- src/Engine/Renderer/VertexBuffer.h
+++ src/Engine/Renderer/VertexBuffer.h
@@ -1,17 +1,39 @@
 
 #pragma once
 
+#include "Renderer.h"
+
+#include <array>
 
 class VertexBuffer {
 private:
+    // Member Variables
     unsigned int m_RendererID;
 
 public:
+    // Constructors
     VertexBuffer(const void *data, unsigned int size);
+    // Overload for std::array use
+    template<typename T, size_t N>
+    explicit VertexBuffer(const std::array<T, N> &vb);
+
+    // Destructor
     ~VertexBuffer();
 
+    // Member Functions
     void Bind() const;
     void Unbind() const;
 
 };
+
+// Templated Constructor for std::array
+template<typename T, size_t N>
+VertexBuffer::VertexBuffer(const std::array<T, N> &vb) {
+    // Generates 1 buffer object name (ID) into m_RendererID
+    glGenBuffers(1, &m_RendererID);
+    // Binds the Vertex Buffer (ID/m_RendererID) to the OpenGL state
+    glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+    // Creates/initializes the bound Vertex Buffer with data of size 'size'
+    glBufferData(GL_ARRAY_BUFFER, vb.size() * sizeof(vb.front()), vb.data(), GL_STATIC_DRAW);
+}
 
