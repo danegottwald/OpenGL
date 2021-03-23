4d0327b5df68e55b4db699277e752c38e8d64c09
Dane Gottwald <danegottwald@gmail.com>
2021-03-23 02:06:08 -0700

    created simple OpenGL render functions wrapper

--- src/Engine/Renderer/Renderer.cpp
+++ src/Engine/Renderer/Renderer.cpp
@@ -11,3 +11,19 @@ ErrorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei le
                   std::endl;
     }
 }
+
+// Binds the Shader, Vertex Array, and the Index Buffer, then issues a Draw Call
+void Renderer::Draw(const VertexArray &va, const IndexBuffer &ib, const Shader &shader) const {
+    // Binds the Shader, Vertex Array (includes Vertex Buffer), and the Index Buffer
+    shader.Bind();
+    va.Bind();
+    ib.Bind();
+
+    // Issue Draw Call
+    glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
+}
+
+// Clears the screen
+void Renderer::Clear() const {
+    glClear(GL_COLOR_BUFFER_BIT);
+}
--- src/Engine/Renderer/Renderer.h
+++ src/Engine/Renderer/Renderer.h
@@ -3,6 +3,10 @@
 
 #include <GL/glew.h>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 void GLAPIENTRY
 ErrorCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message,
               const void *userParam);
@@ -11,6 +15,9 @@ class Renderer {
 private:
 
 public:
+    // Member Functions
+    void Draw(const VertexArray &va, const IndexBuffer &ib, const Shader &shader) const;
+    void Clear() const;
 
 };
 
