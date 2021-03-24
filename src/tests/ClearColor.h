5732b46452bde0b1ceddee7bb7a028a7642eb6dc
Dane Gottwald <danegottwald@gmail.com>
2021-03-24 03:23:54 -0700

    added small unit test

---
+++ src/tests/ClearColor.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "Test.h"
+
+namespace TestSpace {
+
+	class ClearColor : public Test {
+	private:
+		float m_ClearColor[4];
+
+	public:
+		ClearColor();
+		~ClearColor();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImGuiRender() override;
+	};
+
+}
+
---
+++ src/tests/Test.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+namespace TestSpace {
+
+	class Test {
+	private:
+
+	public:
+		Test() {}
+		virtual ~Test() {}
+
+		virtual void OnUpdate(float deltaTime) {}
+		virtual void OnRender() {}
+		virtual void OnImGuiRender() {}
+	};
+
+}
+
+
