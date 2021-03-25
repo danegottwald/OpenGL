a9ee909d111839974c1afbab79999e4703f1aad9
Dane Gottwald <danegottwald@gmail.com>
2021-03-25 03:39:29 -0700

    advanced shader for mvp transforms, textures, colors

--- res/shaders/Basic.glsl
+++ res/shaders/Basic.glsl
@@ -2,15 +2,11 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
-layout(location = 1) in vec2 texCoord;
 
-out vec2 v_TexCoord;
-
-uniform mat4 u_MVP;
+out vec3 ourColor;
 
 void main() {
-    gl_Position = u_MVP * position;
-    v_TexCoord = texCoord;
+    gl_Position = position;
 }
 
 
@@ -20,11 +16,7 @@ void main() {
 
 layout(location = 0) out vec4 color;
 
-in vec2 v_TexCoord;
-uniform sampler2D u_Texture;
-
 void main() {
-    vec4 texColor = texture(u_Texture, v_TexCoord);
-    color = texColor;
+    color = vec4(1.0, 0.0, 0.0, 1.0);
 }
 
